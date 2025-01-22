0c24e9152634acb52d761117043d298c3d67c2f2
zanutt <matszg@live.com>
2025-01-22 16:32:48 -0300

    added games/id endpoint

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,14 +1,11 @@
 package com.devsuperior.dslist.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslist.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
-	List<Game> findAll();
 	
 	
 	
